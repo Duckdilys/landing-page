727620b598d1edbe3ad416f9a4b2f48cdde1fc2f
Brian07 <hoanganh0722@hanh0722.local>
2021-11-06 15:22:58 +0700

    add upload CV and progress

--- components/JobDetail/FormCV/FormCV.js
+++ components/JobDetail/FormCV/FormCV.js
@@ -2,22 +2,19 @@ import React, { useCallback, useEffect, useState } from "react";
 import styles from "./FormCV.module.scss";
 import { Grid, DropzoneUpload, Button } from "../../container";
 import Input from "./Input";
+import Image from "next/image";
 import { useSelector, useDispatch } from "react-redux";
 import { ValidateLengthInput } from "../../../util";
 import useFetch from "../../../hook/use-fetch";
 import { modelActions } from "../../../store/slices/model-slice";
+import Link from 'next/link';
 const FormCV = () => {
   const dispatch = useDispatch();
   const [file, setFile] = useState(null);
+  const [isLoadingSession, setIsLoadingSession] = useState(false);
   const isOpened = useSelector((state) => state.model.openOverlay);
-  const {
-    fetchDataFromServer,
-    error,
-    upload_progress,
-    isLoading,
-    download_progress,
-    data,
-  } = useFetch();
+  const { fetchDataFromServer, error, upload_progress, isLoading, data } =
+    useFetch();
   const submitFormHandler = (event) => {
     event.preventDefault();
   };
@@ -38,8 +35,22 @@ const FormCV = () => {
     },
     [fetchDataFromServer]
   );
-  console.log(file);
-  console.log(error, upload_progress, isLoading, download_progress, data);
+  useEffect(() => {
+    if (isLoading) {
+      setIsLoadingSession(true);
+    }
+    if(!isLoading && !error){
+      setTimeout(() => {
+        setIsLoadingSession(false);
+      }, 1000);
+    }
+    if(!isLoading && error){
+      setFile(null);
+    }
+  }, [isLoading, error]);
+  const removeFileHandler = () => {
+    setFile(null);
+  }
   return (
     <form
       onSubmit={submitFormHandler}
@@ -107,10 +118,47 @@ const FormCV = () => {
           }}
           getFilesHandler={getFileHandler}
         />
+
+        {file && (
+          <div
+            className={`position-relative d-inline-flex align-items-center ${
+              styles.upload
+            } ${isLoadingSession && styles["upload-disabled"]}`}
+          >
+            <span onClick={removeFileHandler} className={styles['remove-file']}><Image src="/close-icon.svg" alt="" width="15px" height="15px"/></span>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="folder"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="svg-inline--fa fa-folder fa-w-16 fa-3x"
+            >
+              <path
+                fill="currentColor"
+                d="M464 128H272l-54.63-54.63c-6-6-14.14-9.37-22.63-9.37H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48zm0 272H48V112h140.12l54.63 54.63c6 6 14.14 9.37 22.63 9.37H464v224z"
+                className=""
+              ></path>
+            </svg>
+            {file && <p className={styles.text}>{file}</p>}
+            {isLoadingSession && (
+              <span
+                className={`${styles.loading}`}
+                style={{ width: `${upload_progress}%` }}
+              >
+                <span className={styles.progress}></span>
+              </span>
+            )}
+          </div>
+        )}
+        {!isLoadingSession && error && <p className={styles.error}>CV không thể upload, xin thử lại sau</p>}
         <div className={styles.button}>
           <Button
             options={{
               type: "submit",
+              disabled: isLoadingSession,
             }}
           >
             Nộp hồ sơ ngay
