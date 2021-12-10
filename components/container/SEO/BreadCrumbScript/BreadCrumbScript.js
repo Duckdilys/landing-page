46dddd35777244e2ffc0fc865e6c508d5e4d6254
Brian07 <hoanganh0594abc@gmail.com>
2021-12-10 08:07:21 +0700

    update header

--- components/container/SEO/BreadCrumbScript/BreadCrumbScript.js
+++ components/container/SEO/BreadCrumbScript/BreadCrumbScript.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import ReactHtmlParser from "react-html-parser";
 import paths from "../../../Navigation/path";
-const BreadCrumbScript = ({ dataElement, title, children }) => {
+const BreadCrumbScript = ({ dataElement, title, children, description }) => {
   const structureList = {
     "@context": "https://schema.org",
     "@type": "BreadcrumbList",
@@ -24,6 +24,9 @@ const BreadCrumbScript = ({ dataElement, title, children }) => {
       {/* <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js"></script> */}
       <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32"/>
       <meta property="og:image" name="og:image" content="https://cdn.noron.vn/2021/12/09/446298a7-7b0a-437f-b313-41e59acffd85-1639048445.png"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content={title}/>
+      <meta property="fb:app_id" content="292204241285034"/>
       {dataElement && (
         <script type="application/ld+json">
           {ReactHtmlParser(JSON.stringify(structureList))}
