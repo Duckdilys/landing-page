a1f27b991bb27748fea17fe412c9f478551d408f
Brian07 <hoanganh0594abc@gmail.com>
2022-03-17 18:40:06 +0700

    fixbug

--- components/Introduction/Banner/Banner.js
+++ components/Introduction/Banner/Banner.js
@@ -5,6 +5,7 @@ const Banner = ({ data }) => {
   return (
     <>
       <BannerPage
+        style={{background: data?.cover_url ? `url(${data?.cover_url})` : ''}}
         classNameBanner={styles.container}
         classNameBox={styles["box-container"]}
         title={data?.title || "Giới thiệu về chúng tôi"}
