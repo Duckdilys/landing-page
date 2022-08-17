61df4725d2823840b35f3aa7afcd1968cc8d84e0
cuongnb <cuongnguyenba94@gmail.com>
2022-08-17 17:48:50 +0700

    change config mh-digital

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#docker build -f Dockerfile -t mhhub.mhsolution.vn/mh_landing_page:v1.0.0 .
-#docker push mhhub.mhsolution.vn/mh_landing_page:v1.0.0
+docker build -f Dockerfile -t mh-registry:5555/mh_digital_landing_page:v1.0.0 .
+docker push mh-registry:5555/mh_digital_landing_page:v1.0.0
 
-ssh centos@18.140.237.112 "docker pull mhhub.mhsolution.vn/mh_landing_page:v1.0.0"
-ssh centos@18.140.237.112 "docker rm -f mh-lading-page"
-ssh centos@18.140.237.112 "docker run --name mh-lading-page -p 3005:3000 -d mhhub.mhsolution.vn/mh_landing_page:v1.0.0"
-ssh centos@18.140.237.112 "docker images | grep 'mhhub' |  xargs docker rmi"
+#ssh centos@18.140.237.112 "docker pull mhhub.mhsolution.vn/mh_landing_page:v1.0.0"
+#ssh centos@18.140.237.112 "docker rm -f mh-lading-page"
+#ssh centos@18.140.237.112 "docker run --name mh-digital-lading-page -p 9703:3000 -d mh-registry:5555/mh_digital_landing_page:v1.0.0"
+#ssh centos@18.140.237.112 "docker images | grep 'mhhub' |  xargs docker rmi"
--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
-export const serverURL = 'https://api.mhsolution.vn/api';
+export const serverURL = 'https://api.mhdigital.vn/api';
 // process.env.SERVER_URL
