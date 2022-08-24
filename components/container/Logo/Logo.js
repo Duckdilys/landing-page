f3efed6479a4042a82a8d715390ab7402c954210
kenjius01 <63000038+kenjius01@users.noreply.github.com>
2022-08-24 11:17:25 +0700

    edit logo

--- components/container/Logo/Logo.js
+++ components/container/Logo/Logo.js
@@ -1,18 +1,20 @@
-import React from "react";
-import useMedia from "../../../hook/use-media";
+import React from 'react';
+import useMedia from '../../../hook/use-media';
 import Image from 'next/image';
 const Logo = (props) => {
-  const match = useMedia('(max-width: 991px)');
-  return (
-    <Image
-      className={props.className}
-      src={match ? "/logo-icon-svg.svg" : "/Logo-icon.svg"}
-      alt="logo"
-      width={match ? "46.67" : "80"}
-      height={match ? "32" : "80"}
-      {...props}
-    />
-  );
+    const match = useMedia('(max-width: 991px)');
+    return (
+        <Image
+            className={props.className}
+            decoding="async"
+            src={'/logo-icon.svg'}
+            alt='logo'
+            width={match ? '46.67' : '80'}
+            height={match ? '32' : '80'}
+            priority
+            {...props}
+        />
+    );
 };
 
 export default Logo;
