eb6bf5275805ccecd16810859fe2193a7f025a50
cuongnb <cuongnguyenba94@gmail.com>
2022-09-18 10:14:13 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 docker build -f Dockerfile -t mh-registry:5555/mh_digital_landing_page:v1.0.1 .
 docker push mh-registry:5555/mh_digital_landing_page:v1.0.1
 
+ssh root@174.138.26.5 'sh /root/restart_fe.sh'
 #ssh centos@18.140.237.112 "docker pull mhhub.mhsolution.vn/mh_landing_page:v1.0.0"
 #ssh centos@18.140.237.112 "docker rm -f mh-lading-page"
 #ssh centos@18.140.237.112 "docker run --name mh-digital-lading-page -p 9703:3000 -d mh-registry:5555/mh_digital_landing_page:v1.0.0"
