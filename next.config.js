7a927a100c54f4a801de29a7192378bb112c61db
LeTri234 <letri234@outlook.com>
2023-08-11 15:05:45 +0700

    fix/fix-serialize-normal-text

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['cdn.noron.vn', 'versatica.ai'],
+    domains: ["cdn.noron.vn", "versatica.ai", "cdn.mhdigital.vn"],
   },
   publicRuntimeConfig: {
     MH_SOLUTION_URL: process.env.MH_SOLUTION_URL,
     MH_SOLUTION_API_URL: process.env.REACT_APP_SERVER_URL,
-    MH_SOLUTION_API_IP: process.env.REACT_APP_SERVER_IP
+    MH_SOLUTION_API_IP: process.env.REACT_APP_SERVER_IP,
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: {
         test: /\.(js|ts)x?$/,
-       // for webpack 5 use
-       // { and: [/\.(js|ts)x?$/] }
+        // for webpack 5 use
+        // { and: [/\.(js|ts)x?$/] }
       },
-      
-      use: ['@svgr/webpack'],
+
+      use: ["@svgr/webpack"],
     });
     return config;
-  }
-}
+  },
+};
