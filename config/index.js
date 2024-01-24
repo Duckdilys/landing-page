46fed00230409cf2797586c50b2a8a52696e9753
kenjius01 <kenjius01@gmail.com>
2024-01-24 10:15:59 +0700

    fix change url data mining

--- config/index.js
+++ config/index.js
@@ -2,3 +2,7 @@ import getConfig from 'next/config'
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 export const serverURL =  publicRuntimeConfig.MH_SOLUTION_API_URL;
 // process.env.SERVER_URL
+export const VERSATICA_APP = {
+    link: 'https://versatica.io',
+    seo_id: 'nen-tang-phan-tich-du-lieu-versatica-data-mining-platform-5'
+}
